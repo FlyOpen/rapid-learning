869fce93a87312d7409fbeb6bd272ac6d49908ba
Flyturtle <aitelstudio@gmail.com>
2018-10-28 15:23:57 +0100

    Ajout du middleware auth a toutes les pages

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])
+    ->group(function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+
